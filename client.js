f9bad800b63e562cb967fcb32b6588e39213ac53
Jakub Sedlacek <sedlacek.jakub@gmail.com>
2017-09-11 22:36:27 +0200

    reproduce with just xhook

--- client.js
+++ client.js
@@ -1,7 +1,7 @@
 const superagent = require('superagent');
 const assert = require('assert');
 
-require('xdomain'); // commenting this line will make the assertion below pass
+require('xhook'); // commenting this line will make the assertion below pass
 
 superagent.get('/api').then(res => {
   assert.equal(res.header.z, 'z');
